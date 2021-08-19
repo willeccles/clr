c79d60b4fff0d600266ddef90a4c633c90245674
Will Eccles <will@eccles.dev>
2021-08-19 15:11:15 -0400

    format source

--- clr.c
+++ clr.c
@@ -12,49 +12,48 @@ static const char* bot = "  \033[3%dm ▀▀▀▀▀\033[3%dm▀\033[m";
 static uint8_t colors;
 
 static void po(void) {
-    char* coloropts = getenv("CLR_OPT");
-    if (!coloropts) {
-        colors = 0x7E;
+  char* coloropts = getenv("CLR_OPT");
+  if (!coloropts) {
+    colors = 0x7E;
+  } else {
+    if (!strlen(coloropts) || !strcmp(coloropts, "*") ||
+        !strcasecmp(coloropts, "ALL")) {
+      colors = 0xFF;
     } else {
-        if (!strlen(coloropts)
-                || !strcmp(coloropts, "*")
-                || !strcasecmp(coloropts, "ALL")) {
-            colors = 0xFF;
+      colors = 0;
+      char* opt;
+      while ((opt = strtok(coloropts, ":"))) {
+        int i = atoi(opt);
+        if (i < 0 || i > 7) {
+          fprintf(stderr, "CLR_OPT: unknown color option: %d\n", i);
         } else {
-            colors = 0;
-            char* opt;
-            while ((opt = strtok(coloropts, ":"))) {
-                int i = atoi(opt);
-                if (i < 0 || i > 7) {
-                    fprintf(stderr, "CLR_OPT: unknown color option: %d\n", i);
-                } else {
-                    colors |= 1 << i;
-                }
-                // see strtok(3)
-                coloropts = NULL;
-            }
+          colors |= 1 << i;
         }
+        // see strtok(3)
+        coloropts = NULL;
+      }
     }
+  }
 }
 
 static void pr(const char* r) {
-    for (int i = 0; i < 8; i++) {
-        if (colors & (1 << i)) {
-            printf(r, i, i);
-        }
+  for (int i = 0; i < 8; i++) {
+    if (colors & (1 << i)) {
+      printf(r, i, i);
     }
-    printf("\n");
+  }
+  printf("\n");
 }
 
 int main(void) {
-    po();
-    if (colors) {
-        printf("\n");
-        pr(top);
-        pr(mid);
-        pr(mid);
-        pr(bot);
-        printf("\n");
-    }
-    return 0;
+  po();
+  if (colors) {
+    printf("\n");
+    pr(top);
+    pr(mid);
+    pr(mid);
+    pr(bot);
+    printf("\n");
+  }
+  return 0;
 }
